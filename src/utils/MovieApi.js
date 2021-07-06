34aeb31a019ba41cb9369e1a37212bf8125b497a
Elena Ganovich <e.ganovich@yandex.ru>
2021-07-06 20:59:28 +0300

    Описан класс MoviesApi

---
+++ src/utils/MovieApi.js
@@ -0,0 +1,25 @@
+class MoviesApi {
+    constructor({ baseUrl }) {
+      this._baseUrl = baseUrl;
+    }
+  
+    _handleOriginalResponse(res) {
+      if (!res.ok) {
+        return Promise.reject(`Ошибка: ${res.status}`);
+      }
+  
+      return res.json();
+    }
+  
+    getMovies() {
+      return fetch(this._baseUrl)
+        .then(res => this._handleOriginalResponse(res));
+    }
+  }
+  
+  
+  const moviesApi = new MoviesApi({
+    baseUrl: 'https://api.nomoreparties.co/beatfilm-movies',
+  });
+  
+  export default moviesApi;
